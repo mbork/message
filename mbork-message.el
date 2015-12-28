0cefa5305d7931b655b66defeba5332d45bccc70
Marcin Borkowski <mbork@mbork.pl>
2015-12-28 23:08:20 +0100

    Initial commit (file template)

---
+++ mbork-message.el
@@ -0,0 +1,30 @@
+;;; mbork-message.el --- mbork's helper functions for message-mode  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2015  Marcin Borkowski
+
+;; Author: Marcin Borkowski <mbork@amu.edu.pl>
+;; Keywords: mail
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; This small library is a set of helper functions to write emails
+;; more effectively.  Currently it is WiP, but quite a few functions
+;; here are already useful.  Note: many things are assumed, like that
+;; ">" is the quotation prefix.  Comments welcome.
+
+;;; Code:
+
+;;; mbork-message.el ends here
